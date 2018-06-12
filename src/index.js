1ca83efb5165bf55e2f0335b1e73b006fb372f2e
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2018-06-12 01:10:41 -0400

    Simple feed query.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
 const { GraphQLServer } = require('graphql-yoga');
 
 const typeDefs = `
-type Query {
-  info: String!
-}
-`
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+
+`;
+
+const links = [{
+  id: 'link-0',
+  url: 'ali-r.com',
+  description: 'My portfolio',
+}];
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-}
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links,
+  },
+
+  // Can be omitted since resolver is so trivial
+  Link: {
+    id: root => root.id,
+    description: root => root.description,
+    url: root => root.url,
+  },
+};
 
 const server = new GraphQLServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
-server.start(() => console.log(`Server is running on http://localhost: 4000`));
+server.start(() => console.log('Server is running on http://localhost: 4000'));
 
