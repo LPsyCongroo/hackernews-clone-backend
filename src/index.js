8758fccd49f63a9fb12f9cae726258aec2b0c584
Ali Rehmatullah <rehmatullah.ali@gmail.com>
2018-06-26 22:43:12 -0400

    Extracted Schema to external file and added a POST mutation type

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-
-`;
-
 const links = [{
   id: 'link-0',
   url: 'ali-r.com',
   description: 'My portfolio',
 }];
+
+let idCount = links.length;
 
 const resolvers = {
   Query: {
@@ -26,16 +14,29 @@ const resolvers = {
     feed: () => links,
   },
 
-  // Can be omitted since resolver is so trivial
+  // Can be omitted since resolver is trivial and the server can infer what it looks like.
   Link: {
     id: root => root.id,
     description: root => root.description,
     url: root => root.url,
   },
+
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  // typeDefs can be provided either directly as a string or by referencing a file that contains your schema definition
+  typeDefs: './src/schema.graphql',
   resolvers,
 });
 server.start(() => console.log('Server is running on http://localhost: 4000'));
